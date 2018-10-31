81f9bde78449c0a345f29df11b1fba2d4101bc3f
Riccardo Cafagna <riccafagna@gmail.com>
2018-10-31 15:36:01 -0300

    Initial Commit

---
+++ Xadrez/src/Xadrez/Torre.java
@@ -0,0 +1,16 @@
+package Xadrez;
+
+public class Torre extends Peca {
+
+	public Torre(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public void Mover() {
+		System.out.println("L");
+	}
+	
+	
+	
+}
