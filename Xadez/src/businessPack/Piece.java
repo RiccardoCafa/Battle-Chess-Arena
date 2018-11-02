31818472d33d59390244ac2ebb910145c493ce0b
MVFalcao <42094176+MVFalcao@users.noreply.github.com>
2018-11-02 15:08:34 -0300

    Lennin
    Add lenin

--- Xadez/src/businessPack/Piece.java
+++ Xadez/src/businessPack/Piece.java
@@ -38,7 +38,6 @@ public abstract class Piece {
     public boolean getLife(){
         return alive;
     }
-    
    // getter for the variable Image
     public Image getMyImage(Image myImage){
         return myImage;
