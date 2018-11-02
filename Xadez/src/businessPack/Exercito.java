fa3896b9cc0c09cee4a6d37ac46e2b79814dbcb4
zikawhite <43191671+zikawhite@users.noreply.github.com>
2018-11-02 17:35:29 -0300

    add player e exercito
    adicionados esperando um getpiece para cemiterio e player esperando mais ideias

---
+++ Xadez/src/businessPack/Exercito.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessPack;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author patri
+ */
+public class Exercito {
+    ArrayList<cemiterio> listPiecesDead = new ArrayList<cemiterio>();
+
+    private static class cemiterio {
+
+        public cemiterio() {
+        }
+    }
+}
