bce816aae21975fb0a9bbd732d140760385050b2
oLucasRez <Lucas_rez@outlook.com>
2018-11-08 16:21:00 -0300

    correção Sheriff

--- Xadez/src/businessPack/Pieces/Sheriff/SheriffHorse.java
+++ Xadez/src/businessPack/Pieces/Sheriff/SheriffHorse.java
@@ -9,6 +9,27 @@ public class SheriffHorse implements ItypeHorse {
     //metodos>>
     @Override
     public ArrayList<Vetor> IcheckMove(Table table, Vetor vetor) {//implementação do cavalo especial do Sheriff
-        return null;
+        ArrayList<Vetor> freeWay = new ArrayList<>();
+        freeWay.add(new Vetor(vetor.getX() - 1, vetor.getY() - 2));
+        freeWay.add(new Vetor(vetor.getX() - 1, vetor.getY() + 2));
+        freeWay.add(new Vetor(vetor.getX() - 2, vetor.getY() + 1));
+        freeWay.add(new Vetor(vetor.getX() - 2, vetor.getY() - 1));
+        freeWay.add(new Vetor(vetor.getX() + 1, vetor.getY() - 2));
+        freeWay.add(new Vetor(vetor.getX() + 1, vetor.getY() + 2));
+        freeWay.add(new Vetor(vetor.getX() + 2, vetor.getY() + 1));
+        freeWay.add(new Vetor(vetor.getX() + 2, vetor.getY() - 1));
+        for(Vetor position : freeWay){
+            if(position.getX() < 0 || position.getX() > table.getM() ||
+               position.getY() < 0 || position.getY() > table.getN()){
+                freeWay.remove(position);
+            }
+        }
+        for(Vetor position : freeWay){
+            if(table.getBlock(position.getX(), position.getY()).getPiece().getTpHero() ==
+               table.getBlock(   vetor.getX(), vetor.getY()).getPiece().getTpHero()){
+                freeWay.remove(position);
+            }
+        }
+        return freeWay;
     }
 }
