43eaa79a59407f2b3f20de812ce8eee7f4225d46
oLucasRez <Lucas_rez@outlook.com>
2018-11-08 15:23:56 -0300

    organizado os heros

--- Xadez/src/businessPack/Heros/Huebr.java
+++ Xadez/src/businessPack/Heros/Huebr.java
@@ -2,17 +2,17 @@ package businessPack.Heros;
 
 import businessPack.Army;
 import businessPack.Hero;
-import businessPack.Pieces.King;
+import extras.Vetor;
 import javafx.scene.image.Image;
 
 public class Huebr extends Hero {
-
+    //construtor>>
     public Huebr(Image image) {
         super(image);
     }
-
+    //metodos>>
     @Override
-    public void createArmy(Army army) {
+    public void createArmy(Army army, int sentido) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     ///HU3HU3HU3HU3 BRBRBRB
