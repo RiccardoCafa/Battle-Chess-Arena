0f209895314cce6f1e3a433b24a910e8463509fe
Lucas Souza <souzacontato18@gmail.com>
2018-11-11 02:13:18 -0300

    add Wizard pieces
    Add a package for wizard
    add 3 new classes
    implemented 10 pieces on Wizard's army

--- Xadez/src/businessPack/Heros/Wizard.java
+++ Xadez/src/businessPack/Heros/Wizard.java
@@ -2,16 +2,34 @@ package businessPack.Heros;
 
 import businessPack.Army;
 import businessPack.Hero;
+import businessPack.Pieces.Bishop;
+import businessPack.Pieces.Peon;
+import businessPack.Pieces.Wizard.WizardBishop;
+import businessPack.Pieces.Wizard.WizardPeon;
+import businessPack.TypeHero;
 import javafx.scene.image.Image;
 
 public class Wizard extends Hero {
-
+    
+    // atributos
+    Image wizardBishop;
+    Image wizardPeon;
+    Image wizardKing;
+    Image wizardQueen;
+    Image wizardHorse;
+    Image wizardTower;
+    
     public Wizard(Image image) {
         super(image);
     }
 
     @Override
     public void createArmy(Army army, int sentido) {
+        for(int k = 0;k<8;k++){
+            army.addPiece(new Peon(new WizardPeon(), TypeHero.wizard, k, (int)(3.5 + sentido*2.5), wizardPeon)); //peões
+        }
+        army.addPiece(new Bishop(new WizardBishop(), TypeHero.wizard, 2, (int)(3.5 + sentido*2.5), wizardBishop));// Bispos 
+        army.addPiece(new Bishop(new WizardBishop(), TypeHero.wizard, 5, (int)(3.5 + sentido*2.5), wizardBishop));// Bispos 
 
         
     }
---
+++ Xadez/src/businessPack/Pieces/Wizard/WizardPeon.java
@@ -0,0 +1,20 @@
+package businessPack.Pieces.Wizard;
+
+import businessPack.Block;
+import businessPack.Pieces.Interfaces.ItypePeon;
+import businessPack.Table;
+import extras.Vetor;
+import java.util.ArrayList;
+
+
+public class WizardPeon implements ItypePeon {
+
+    @Override
+    public ArrayList<Block> IcheckMove(Table table, Vetor vetor) {
+        return null;
+    }
+
+
+    
+    
+}
