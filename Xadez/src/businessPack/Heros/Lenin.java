89c78d34e33a74a3e0a6ced0eaef781d3dbbc8bd
MVFalcao <42094176+MVFalcao@users.noreply.github.com>
2018-11-11 13:51:51 -0300

    Lenin army

--- Xadez/src/businessPack/Heros/Lenin.java
+++ Xadez/src/businessPack/Heros/Lenin.java
@@ -2,14 +2,27 @@ package businessPack.Heros;
 
 import businessPack.Army;
 import businessPack.Hero;
+import businessPack.Pieces.Bishop;
+import businessPack.Pieces.Default.DefaultPeon;
+import businessPack.Pieces.Default.DefaultQueen;
+import businessPack.Pieces.King;
+import businessPack.Pieces.Lenin.LeninBishop;
+import businessPack.Pieces.Lenin.LeninKing;
 import businessPack.Pieces.Tower;
 import businessPack.Pieces.Lenin.LeninTower;
+import businessPack.Pieces.Peon;
+import businessPack.Pieces.Queen;
 import businessPack.TypeHero;
 import javafx.scene.image.Image;
 
 public class Lenin extends Hero {
     //atributos>>
     Image leninTowerImage;
+    Image LeninBishopImage;
+    Image LeninPeonImage;
+    Image LeninKingImage;
+    Image LeninQueenImage;
+    Image LeninHorseImage;
     //construtor>>
     public Lenin(Image image) {
         super(image);
@@ -17,6 +30,15 @@ public class Lenin extends Hero {
     //metodos>>
     @Override
     public void createArmy(Army army, int sentido){
-        army.addPiece(new Tower(new LeninTower(), TypeHero.lenin, 0, 0, leninTowerImage));
+        for(int k = 0;k<8;k++){
+            army.addPiece(new Peon(new DefaultPeon(), TypeHero.lenin, k, (int)(3.5 + sentido*2.5), LeninPeonImage)); //peões
+        }
+        army.addPiece(new Bishop(new LeninBishop(), TypeHero.lenin, 2, (int)(3.5 + sentido*2.5), LeninBishopImage));// Bispos 
+        army.addPiece(new Bishop(new LeninBishop(), TypeHero.lenin, 5, (int)(3.5 + sentido*2.5), LeninBishopImage));// Bispos 
+        army.addPiece(new Tower(new LeninTower(), TypeHero.lenin, 0, (int)(3.5 + sentido*2.5), leninTowerImage));
+        army.addPiece(new Tower(new LeninTower(), TypeHero.lenin, 7, (int)(3.5 + sentido*2.5), leninTowerImage));
+        army.addPiece(new King(new LeninKing(), TypeHero.lenin, 3, (int)(3.5 + sentido*2.5), LeninKingImage));
+        army.addPiece(new Queen(new DefaultQueen(), TypeHero.lenin, 4, (int)(3.5 + sentido*2.5), LeninQueenImage));
+        
     }
 }
