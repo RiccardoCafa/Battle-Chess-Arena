5b1236e4ca6ecaa14d980191c042aa129cedb440
Lucas Souza <souzacontato18@gmail.com>
2018-11-30 17:58:08 -0300

    Erros dos Lucas

--- Xadez/src/businessPack/Pieces/Sheriff/PistolSound.java
+++ Xadez/src/businessPack/Pieces/Sheriff/PistolSound.java
@@ -1,23 +1,16 @@
 package businessPack.Pieces.Sheriff;
 
-import java.io.File;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class PistolSound {
     //atributos>>
-    private String[] paths = {
-        new File("src/businessPack/Pieces/Sheriff/rechargeSound.mp3").getAbsolutePath(),
-        new File("src/businessPack/Pieces/Sheriff/shootSound.mp3").getAbsolutePath()
-    };
-    Media[] sounds = {
-        new Media(new File(paths[0]).toURI().toString()),
-        new Media(new File(paths[1]).toURI().toString())};
-        //new Media(getClass().getResource("rechargeSound.mp3").toString()),
-        //new Media(getClass().getResource("shootSound.mp3").toString())};
-    MediaPlayer[] soundBox = {
-        new MediaPlayer(sounds[0]), new MediaPlayer(sounds[1])};
+    Media rec = new Media(getClass().getResource("rechargeSound.mp3").toString());
+    Media shoot = new Media(getClass().getResource("shootSound.mp3").toString());
+    MediaPlayer shootSound = new MediaPlayer(shoot);
+    MediaPlayer recharge = new MediaPlayer(rec);
+    
     //metodos>>
-    public void playRechargeSound(){ soundBox[0].play(); }
-    public void playShootSound(){ soundBox[1].play(); }
+    public void playRechargeSound(){ recharge.play(); }
+    public void playShootSound(){ shootSound.play(); }
 }
