443e77eb74fb8f76b6c05d4fb0945f3274572d32
Riccardo Cafagna <riccafagna@gmail.com>
2018-12-03 00:17:44 -0300

    Update ChooseCharacterController.java

--- Xadez/src/InterfaceView/ChooseCharacterController.java
+++ Xadez/src/InterfaceView/ChooseCharacterController.java
@@ -1,5 +1,6 @@
 package InterfaceView;
 
+import businessPack.Hero;
 import businessPack.Heros.Huebr;
 import businessPack.Heros.Lapa;
 import businessPack.Heros.Lenin;
@@ -71,6 +72,7 @@ public class ChooseCharacterController implements Initializable {
     Image myImage;
     ImageView myImageView;
     int count = 0,cont = 0;
+    int countAnt = -1;
     Image[] perso = new Image[5];
     String[] heroNames = new String[5];
     Player p1,p2;
@@ -108,30 +110,45 @@ public class ChooseCharacterController implements Initializable {
         }else{
             count--;
         }
-//        musicaAtual = musicas.get(count);
-//        musicaAtual.setStartTime(Duration.ZERO);
-//        musicaAtual.isAutoPlay();
-//        musicaAtual.play();
-        heroName.setText(heroNames[count]);
-        characterSelection.setGraphic(new ImageView(perso[count]));
+        if(countAnt == count) {
+            count--;
+            if(count<0) count = 4;
+        }
+        changeCharImage();
     }
     @FXML
     public void onSetaClickDir(MouseEvent event){
-
         if(count == 4){
             count = 0;
         }else{
             count++;
         }
+        if(countAnt == count) {
+            count++;
+            if(count > 4) count = 0;
+        }
+        changeCharImage();
 //        musicaAtual = musicas.get(count);
 //        musicaAtual.setStartTime(Duration.ZERO);
 //        musicaAtual.isAutoPlay();
 //        musicaAtual.play();
+        
+    }
+    
+    public void changeCharImage() {
         heroName.setText(heroNames[count]);
         characterSelection.setGraphic(new ImageView(perso[count]));
     }
     @FXML
     public void onClickButton(MouseEvent e){
+        System.out.println(name1);
+        System.out.println(name2);
+        
+        if(!setName()) {
+            JOptionPane.showMessageDialog(null, "Nomes iguais!");
+            return;
+        }
+        
         switch(count){
             case 0:
                 //name = "Hue";
@@ -209,6 +226,11 @@ public class ChooseCharacterController implements Initializable {
                 } 
                 break;
         }
+        countAnt = count;
+        count++;
+        if(count > 4) count = 0;
+        changeCharImage();
+        names1.setText("");
         //funcao para pegar o player
         if(p1 != null && p2 != null){
             primaryStage = (Stage) background.getScene().getWindow();
@@ -235,30 +257,30 @@ public class ChooseCharacterController implements Initializable {
             e.printStackTrace();
         }
     }
-      @FXML
-      public void onEnterClick(KeyEvent e){
-          if(e.getCode() == KeyCode.ENTER){
-              if(cont == 0){
-                if("".equals(names1.getText())){
-                   System.out.println(name1);
-                   cont++;
-                }else{
-                  name1 = names1.getText();
-                  System.out.println(name1);
-                  PlayerName1.setText(name1);
-                  cont++;
-                }     
-              }else{
-                  if("".equals(names1.getText())){
-                   System.out.println(name2);
-              }else{
-                  name2 = names1.getText();
-                  System.out.println(name2);
-                  PlayerName2.setText(name2);
-              }
-              }
-              
-          }
-      }
+    @FXML
+    public void onEnterClick(KeyEvent e){
+        if(e.getCode() == KeyCode.ENTER){
+            setName();
+        }
+    }
+    
+    public boolean setName() {
+        if(cont == 0){
+            if(!names1.getText().equals("")){
+              name1 = names1.getText();
+              PlayerName1.setText(name1);
+            }
+            cont++;
+        }else{
+            if(!names1.getText().equals("")){
+                name2 = names1.getText();
+                if(name1.compareToIgnoreCase(name2) == 0) {
+                    return false;
+                }
+                PlayerName2.setText(name2);
+            }
+        }
+        return true;
+    }
 
 }
