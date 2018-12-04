f133e96c0b80b4fd88c3bf5c5e3c4226a3e519a9
Riccardo Cafagna <riccafagna@gmail.com>
2018-12-04 11:37:01 -0300

    Option menu e Icone
    Co-Authored-By: Miguel Falcão  <mvfalcao@users.noreply.github.com>

--- Xadez/src/InterfaceView/OptionMenuController.java
+++ Xadez/src/InterfaceView/OptionMenuController.java
@@ -1,14 +1,21 @@
 package InterfaceView;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -20,10 +27,35 @@ public class OptionMenuController implements Initializable {
     Button QuitBtn;
     @FXML
     AnchorPane rootPane;
+    @FXML
+    Slider volumeSlider;
+    @FXML
+    Button SaveBtn;
     private Stage primaryStage;
+    private File optionFile;
+    private File gameFolder;
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        new File(System.getProperty("java.io.tmpdir") + "BattleChessArena").mkdir();
+        String tempPath = System.getProperty("java.io.tmpdir") + "/BattleChessArena";
+        System.out.println(tempPath);
+        optionFile = new File(tempPath, "options.txt");
+    }
+    @FXML
+    public void onOptionsSave() {
+        
+        try {
+            //if(optionFile.exists()) {
+            FileWriter escrevedor = new FileWriter(optionFile);
+            escrevedor.write("Volume " + volumeSlider.getValue());
+            JOptionPane.showMessageDialog(null, "Configurações salvas!");
+            escrevedor.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(OptionMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
         
     }
     @FXML
